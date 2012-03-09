9a24c1cc41e9e970d0dfe8645d72b4544b71265f
Daniel Tang <dt.tangr@gmail.com>
2012-03-09 20:07:37 +1100

    timers weren't being restored correctly for some reason. removed support temporarily

--- timer.c
+++ timer.c
@@ -4,7 +4,7 @@
 static unsigned timer_load, timer_saved_control, timer_saved_load;
 
 void setup_timer(int fps) {
-    volatile unsigned *control = (unsigned*)0x900D0008;
+    /*volatile unsigned *control = (unsigned*)0x900D0008;
 	volatile unsigned *load = (unsigned*)0x900D0000;
 	volatile unsigned *value = (unsigned*)0x900D0004;
 	if (is_cx) {
@@ -14,11 +14,11 @@ void setup_timer(int fps) {
         *control = 0b01100011; // disabled, TimerMode N/A, int, no prescale, 32-bit, One Shot (for the *value > 0 test) -> 32khz
         *control = 0b11100011; // enable timer
         timer_load = 32000/fps;
-    }
+    }*/
 }
 
 void restore_timer() {
-    volatile unsigned *control = (unsigned*)0x900D0008;
+    /*volatile unsigned *control = (unsigned*)0x900D0008;
 	volatile unsigned *load = (unsigned*)0x900D0000;
 	volatile unsigned *value = (unsigned*)0x900D0004;
 	if (is_cx) {
@@ -26,16 +26,16 @@ void restore_timer() {
         *control = timer_saved_control & 0b01111111; // timer still disabled
         *load = timer_saved_load;
         *control = timer_saved_control; // enable timer
-    }
+    }*/
 }
 
 void tick_timer() {
-    volatile unsigned *control = (unsigned*)0x900D0008;
+    /*volatile unsigned *control = (unsigned*)0x900D0008;
 	volatile unsigned *load = (unsigned*)0x900D0000;
 	volatile unsigned *value = (unsigned*)0x900D0004;
 	if (is_cx) {
 	    *load = timer_load;
 	}
 	while (*value > 0)
-			idle();
+			idle();*/
 }
